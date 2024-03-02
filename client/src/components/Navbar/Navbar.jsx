24a7b6c1e1c72138c9ac8aa4c5e44110e233db7d
Kushagra2478 <159173283+Kushagra2478@users.noreply.github.com>
2024-03-02 15:45:33 +0530

    push (#1)

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul style={{display:'flex',gap:'10px'}}>
+        <li>lotrem</li>
+        <li>lotrem</li>
+
+        <li>lotrem</li>
+        <li>lotrem</li>
+
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
