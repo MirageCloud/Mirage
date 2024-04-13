080a8800a5aa0d14b64ea68bc9c565eebb18fbea
kunal Dugar <yoyokvunal@gmail.com>
2024-04-13 19:04:35 +0530

    Initiated use of Material UI and added sample Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,21 @@
 import './App.css'
 import Btnfooter from './components/Logo/Btnfooter'
 import SpeedTestBtn from './components/Logo/SpeedTestBtn'
+import Navbar from "./components/Navbar/Navbar"
 function App() {
-  
+
 
   return (
     <>
       <div >
-        <SpeedTestBtn/>
+        {/* <SpeedTestBtn/> */}
 
+        <Navbar />
+        {/* <Btnfooter /> */}
 
-        <Btnfooter /> 
-        
       </div>
-      
-  
+
+
     </>
   )
 }
