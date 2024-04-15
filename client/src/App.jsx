625ef8abc10b441bb1b5576182e41526c7160138
kunal Dugar <yoyokvunal@gmail.com>
2024-04-15 14:42:56 +0530

    I have added MaterialUI and also a sample Navbar component all rest Frontend code need to changed accordingly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,22 @@
 
 import './App.css'
-import Btnfooter from './components/Logo/Btnfooter'
-import SpeedTestBtn from './components/Logo/SpeedTestBtn'
+import Btnfooter from './components/Logo/Btnfooter' // need to shift this to material ui
+import SpeedTestBtn from './components/Logo/SpeedTestBtn'  // need to shift this to material ui
+import Navbar from './components/Navbar/Navbar'
 function App() {
-  
+
 
   return (
     <>
-      <div >
+      {/* <div >
         <SpeedTestBtn/>
 
 
         <Btnfooter /> 
         
-      </div>
-      
-  
+      </div> */}
+      <Navbar />
+
     </>
   )
 }
