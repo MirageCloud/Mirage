7bf963f34b2bd82c494093740f6880f461888dfb
Reuben <treuben90@yahoo.com>
2024-05-06 01:59:09 +0300

    changed the Go button background color and assigned it a cursor pointer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,18 @@
-
-import './App.css'
-import Btnfooter from './components/Logo/Btnfooter' // need to shift this to material ui
-import SpeedTestBtn from './components/Logo/SpeedTestBtn'  // need to shift this to material ui
-import Navbar from './components/Navbar/Navbar'
+import "./App.css";
+import Btnfooter from "./components/Logo/Btnfooter"; // need to shift this to material ui
+import SpeedTestBtn from "./components/Logo/SpeedTestBtn"; // need to shift this to material ui
+import Navbar from "./components/Navbar/Navbar";
 function App() {
-
-
   return (
     <>
-      {/* <div >
-        <SpeedTestBtn/>
+      <div>
+        <SpeedTestBtn />
 
-
-        <Btnfooter /> 
-        
-      </div> */}
+        {/* <Btnfooter /> */}
+      </div>
       <Navbar />
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
